f0b2be2ab5ac2358dc734734a915ddb2680aac68
MennaELRouby <roubymenna@gmail.com>
2025-07-16 11:34:04 +0300

    edit image user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,30 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\SaveImageTo3Path;
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
 use App\Models\User;
-use DB;
-use File;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
-use Image;
 use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
 {
-    
+
     public function __construct()
     {
         //$this->middleware('permission:user');
     }
-    
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+
     public function index()
     {
         //
@@ -33,11 +27,7 @@ class UserController extends Controller
         return view('admin.users.users',compact('users'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         //
@@ -45,12 +35,6 @@ class UserController extends Controller
         return view('admin.users.addUser',compact('roles'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
@@ -65,48 +49,10 @@ class UserController extends Controller
         $add->remember_token = $token;
         $add->is_admin = $request->admin;
 
-        if ($request->hasFile("image")) {
-
+        if ( $request->hasFile("image")) {
             $file = $request->file("image");
-            $mime = File::mimeType($file);
-            $mimearr = explode('/', $mime);
-
-            // $destinationPath = public_path() . '/uploads/'; // upload path
-            $extension = $mimearr[1]; // getting file extension
-            $fileName = rand(11111, 99999) . '.' . $extension; // renameing image
-            $path = public_path('uploads/users/source/' . $fileName);
-            $resize200 = public_path('uploads/users/resize200/' . $fileName);
-            $resize800 = public_path('uploads/users/resize800/' . $fileName);
-            //  $file->move($destinationPath, $fileName);
-
-            Image::make($file->getRealPath())->save($path);
-
-            $arrayimage = list($width, $height) = getimagesize($file->getRealPath());
-            $widthreal = $arrayimage['0'];
-            $heightreal = $arrayimage['1'];
-
-            $width200 = ($widthreal / $heightreal) * 200;
-            $height200 = $width200 / ($widthreal / $heightreal);
-
-            $img200 = Image::canvas($width200, $height200);
-            $image200 = Image::make($file->getRealPath())->resize($width200, $height200, function ($c) {
-                $c->aspectRatio();
-                $c->upsize();
-            });
-            $img200->insert($image200, 'center');
-            $img200->save($resize200);
-
-            $width800 = ($widthreal / $heightreal) * 800;
-            $height800 = $width800 / ($widthreal / $heightreal);
-
-            $img800 = Image::canvas($width800, $height800);
-            $image800 = Image::make($file->getRealPath())->resize($width800, $height800, function ($c) {
-                $c->aspectRatio();
-                $c->upsize();
-            });
-            $img800->insert($image800, 'center');
-            $img800->save($resize800);
-
+            $saveImage = new SaveImageTo3Path($file,true);
+            $fileName = $saveImage->saveImages('users');
             $add->image = $fileName;
         }
 
@@ -122,23 +68,7 @@ class UserController extends Controller
         return redirect('admin/users')->with('success',trans('home.your_item_added_successfully'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
@@ -150,16 +80,9 @@ class UserController extends Controller
         return view('admin.users.editUser',compact('user','roles','userRoles'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-       
+
         //
         $update = User::find($id);
         $update->is_admin = $request->admin;
@@ -171,58 +94,11 @@ class UserController extends Controller
             $update->password = bcrypt($request->password);
         }
 
-        if ($request->hasFile("image")) {
-
+        if ( $request->hasFile("image")) {
             $file = $request->file("image");
-            $mime = File::mimeType($file);
-            $mimearr = explode('/', $mime);
-
-            $img_path = public_path() . '/uploads/users/source/';
-            $img_path200 = public_path() . '/uploads/users/resize200/';
-            $img_path800 = public_path() . '/uploads/users/resize800/';
-
-            if ($update->image != null) {
-                unlink(sprintf($img_path . '%s', $update->image));
-                unlink(sprintf($img_path200 . '%s', $update->image));
-                unlink(sprintf($img_path800 . '%s', $update->image));
-            }
-            
-            // $destinationPath = public_path() . '/uploads/'; // upload path
-            $extension = $mimearr[1]; // getting file extension
-            $fileName = rand(11111, 99999) . '.' . $extension; // renameing image
-            $path = public_path('uploads/users/source/' . $fileName);
-            $resize200 = public_path('uploads/users/resize200/' . $fileName);
-            $resize800 = public_path('uploads/users/resize800/' . $fileName);
-            //  $file->move($destinationPath, $fileName);
-
-            Image::make($file->getRealPath())->save($path);
-
-            $arrayimage = list($width, $height) = getimagesize($file->getRealPath());
-            $widthreal = $arrayimage['0'];
-            $heightreal = $arrayimage['1'];
-
-            $width200 = ($widthreal / $heightreal) * 200;
-            $height200 = $width200 / ($widthreal / $heightreal);
-
-            $img200 = Image::canvas($width200, $height200);
-            $image200 = Image::make($file->getRealPath())->resize($width200, $height200, function ($c) {
-                $c->aspectRatio();
-                $c->upsize();
-            });
-            $img200->insert($image200, 'center');
-            $img200->save($resize200);
-
-            $width800 = ($widthreal / $heightreal) * 800;
-            $height800 = $width800 / ($widthreal / $heightreal);
-
-            $img800 = Image::canvas($width800, $height800);
-            $image800 = Image::make($file->getRealPath())->resize($width800, $height800, function ($c) {
-                $c->aspectRatio();
-                $c->upsize();
-            });
-            $img800->insert($image800, 'center');
-            $img800->save($resize800);
-
+            $saveImage = new SaveImageTo3Path($file,true);
+            $fileName = $saveImage->saveImages('users');
+            SaveImageTo3Path::deleteImage(  $update->image, 'users');
             $update->image = $fileName;
         }
 
@@ -239,12 +115,7 @@ class UserController extends Controller
         return redirect('admin/users')->with('success',trans('home.your_item_updated_successfully'));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy($ids)
     {
         //
